ae9172013d3cc30dd86920898af14deda1f07773
aocha <abcdeman00@gmail.com>
2021-10-06 08:51:22 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
 
-    if @task.update(message_params)
+    if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
@@ -45,11 +45,13 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-    
-    
-end
-
+  
+    private
 
   def task_params
     params.require(:task).permit(:content)
   end
+
+    
+    
+end
