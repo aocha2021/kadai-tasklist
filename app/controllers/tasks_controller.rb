abf0cf750bca84ef76bc2f8c9baf58d208c49578
aocha <abcdeman00@gmail.com>
2021-10-11 06:09:02 +0000

    tasklist-heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-   before_action :set_task, only: [:show, :edit, :update, :destroy]
+   before_action :correct_user, only: [:show, :edit, :update, :destroy]
    before_action :require_user_logged_in
   
   def index
@@ -49,14 +49,16 @@ class TasksController < ApplicationController
   
     private
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
   def task_params
     params.require(:task).permit(:content, :status)
   end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
     
     
 end
